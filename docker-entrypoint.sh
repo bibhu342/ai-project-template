7af943587fd4e01f47869e980701f755b944db1a
Bibhudendu Behera <bibhu342@gmail.com>
2025-11-06 17:50:00 +0530

    feat: add SKIP_MIGRATIONS env var to skip alembic migrations

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env sh
 set -e
 
-# run migrations on container start
-alembic upgrade head
+# run migrations on container start (skip if SKIP_MIGRATIONS=1)
+if [ "$SKIP_MIGRATIONS" != "1" ]; then
+  alembic upgrade head
+fi
 
 # start API
 exec uvicorn app.main:app --host 0.0.0.0 --port 8000
